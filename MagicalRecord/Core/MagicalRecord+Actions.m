7e529d5f040689de284257a542f11d6feeec6927
Wendy Kutschke <support@chronicstimulation.com>
2012-07-07 14:18:23 -0400

    Addded a method to save in background using a child of a specified parent NSManagedObjectController.

--- MagicalRecord/Core/MagicalRecord+Actions.m
+++ MagicalRecord/Core/MagicalRecord+Actions.m
@@ -33,7 +33,7 @@ void reset_action_queue(void)
 
 @implementation MagicalRecord (Actions)
 
-+ (void) saveInBackgroundUsingContext:(NSManagedObjectContext *)localContext block:(void (^)(NSManagedObjectContext *))block completion:(void(^)(void))completion errorHandler:(void(^)(NSError *))errorHandler;
++ (void) saveInBackgroundUsingContext:(NSManagedObjectContext *)localContext block:(void (^)(NSManagedObjectContext *localContext))block completion:(void(^)(void))completion errorHandler:(void(^)(NSError *))errorHandler;
 {
     dispatch_async(action_queue(), ^{
         block(localContext);
@@ -57,6 +57,13 @@ void reset_action_queue(void)
     [self saveInBackgroundUsingContext:localContext block:block completion:completion errorHandler:errorHandler];
 }
                                     
++ (void) saveInBackgroundUsingChildOfParentContext:(NSManagedObjectContext *)parentContext WithBlock:(void (^)(NSManagedObjectContext *))block completion:(void (^)(void))completion errorHandler:(void (^)(NSError *))errorHandler;
+{
+    NSManagedObjectContext *localContext = [NSManagedObjectContext MR_contextWithParent:parentContext];
+    
+    [self saveInBackgroundUsingContext:localContext block:block completion:completion errorHandler:errorHandler];
+}
+
 + (void) saveWithBlock:(void (^)(NSManagedObjectContext *localContext))block completion:(void (^)(void))completion errorHandler:(void (^)(NSError *))errorHandler;
 {
     NSManagedObjectContext *mainContext  = [NSManagedObjectContext MR_defaultContext];
