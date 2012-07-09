ef951d3acd75afc4dd424209fee751c303b8eabb
Wendy Kutschke <support@chronicstimulation.com>
2012-07-09 11:12:25 -0400

    Moved secondary operations back into the block.

--- MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalSaves.m
+++ MagicalRecord/Categories/NSManagedObjectContext/NSManagedObjectContext+MagicalSaves.m
@@ -101,18 +101,17 @@
     [self performBlockAndWait:^{
         [self MR_saveWithErrorCallback:errorCallback];
 
+        if (self == [[self class] MR_defaultContext])
+        {
+            [[[self class] MR_rootSavingContext] MR_saveInBackgroundErrorHandler:errorCallback completion:completion];
+            return;
+        }
+        
+        if (completion || self == [[self class] MR_rootSavingContext])
+        {
+            dispatch_async(dispatch_get_main_queue(), completion);
+        }        
     }];
-
-    if (self == [[self class] MR_defaultContext])
-    {
-        [[[self class] MR_rootSavingContext] MR_saveInBackgroundErrorHandler:errorCallback completion:completion];
-        return;
-    }
-    
-    if (completion || self == [[self class] MR_rootSavingContext])
-    {
-        dispatch_async(dispatch_get_main_queue(), completion);
-    }        
 }
 
 @end
